6955b27ff44aa3b43313c392f6d32e4d9502ba2e
KimDaehyeon <daehyeon3351@gmail.com>
2025-06-10 13:58:24 +0900

    feat: Entity 구현

---
+++ src/main/java/finalmission/member/domain/Member.java
@@ -0,0 +1,55 @@
+package finalmission.member.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "member")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Column(name = "name")
+    private String name;
+
+    @NotNull
+    @Column(name = "email")
+    private String email;
+
+    @NotNull
+    @Column(name = "password")
+    private String password;
+
+    public Member(
+            final String name,
+            final String email,
+            final String password
+    ) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "Member{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/finalmission/reservation/domain/Reservation.java
@@ -0,0 +1,78 @@
+package finalmission.reservation.domain;
+
+
+import finalmission.member.domain.Member;
+import finalmission.room.domain.Room;
+import finalmission.time.domain.Time;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import java.time.LocalDate;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "reservation")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Reservation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "room_id")
+    private Room room;
+
+    @NotNull
+    @Column(name = "date")
+    private LocalDate date;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "time_id")
+    private Time time;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+
+    Reservation(final Room room, final LocalDate date, final Time time, final Member member) {
+        this.room = room;
+        this.date = date;
+        this.time = time;
+        this.member = member;
+    }
+
+    public static Reservation makeReservation(
+            final Room room,
+            final LocalDate date,
+            final Time time,
+            final Member member
+    ) {
+        return new Reservation(room, date, time, member);
+    }
+
+    @Override
+    public String toString() {
+        return "Reservation{" +
+                "id=" + id +
+                ", room=" + room +
+                ", date=" + date +
+                ", time=" + time +
+                ", member=" + member +
+                '}';
+    }
+}
